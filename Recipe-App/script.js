50eb9edc74c4a9d7350fa19dc0e43447971bc21c
Sachin <heysachinkry@gmail.com>
2022-05-30 13:03:46 +0530

    recipe-cont

--- Recipe-App/script.js
+++ Recipe-App/script.js
@@ -1,17 +1,21 @@
+const meals = document.getElementById('meals');
+
 getRandomMeal();
 
 async function getRandomMeal() {
     const resp = await fetch(
         'https://www.themealdb.com/api/json/v1/1/random.php'
-    );
+    )
 
     const respData = await resp.json();
     const randomMeal = respData.meals[0];
-
     console.log(randomMeal);
 
     addMeal(randomMeal, true);
+
 }
+
+
 
 async function getMealById() {
     const meal = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
@@ -22,21 +26,32 @@ async function getMealBySearch() {
     const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term);
 }
 
-addMeal(mealdata, random = false) {
+function addMeal(mealData, random = false) {
     const meal = document.createElement('div');
     meal.classList.add('meal');
 
     meal.innerHTML = `
          <div class="meal-header">
-         ${random ?}
-             <span class="random">Random Recipe</span>
-             <img src="https://www.themealdb.com/images/media/meals/tnwy8m1628770384.jpg" alt="">
-         </div>
-         <div class="meal-body">
-             <h4>Veggie Vaggies</h4>
-             <button class="fav-btn">
-                 <i class="fa fa-heart"></i>
-             </button>
-         </div>
-    `
+            ${random ? `
+            <span class="random">Random Recipe</span>
+            `: ''}
+
+            <img 
+               src="${mealData.strMealThumb}"
+               alt="${mealData.strMeal}"
+            />
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button class="fav-btn" id="fav-btn-el" >
+                <i class="fa fa-heart"></i>
+            </button>
+            </div>
+            `;
+    meals.appendChild(meal);
+
+
+    meal.querySelector('.meal-body .fav-btn').addEventListener('click', () => {
+        alert('real is fake and fake is fake');
+    })
 }
